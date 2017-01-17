6d57a0f0a294b998b644323dc43dda7053e55231
Alexia Young <alexia.kwok@gmail.com>
2017-01-17 12:00:36 -0600

    added css path

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const tweetBank = require('../tweetBank.js');
+const path = require('path');
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
+});
+
+router.get('/stylesheets/style.css', (req, res) => {
+  var stylePath = path.join(__dirname, '../public/stylesheets/style.css');
+  res.sendFile(stylePath);
 });
 
 module.exports = router;
